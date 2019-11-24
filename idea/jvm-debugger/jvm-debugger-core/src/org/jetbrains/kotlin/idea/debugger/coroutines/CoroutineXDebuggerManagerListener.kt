0bb4e542cbee366e79869e50d6d8e26433e3cc48
Vladimir Ilmov <vladimir.ilmov@jetbrains.com>
2019-11-24 23:53:26 +0100

    [wip] slight change

--- idea/jvm-debugger/jvm-debugger-core/src/org/jetbrains/kotlin/idea/debugger/coroutines/CoroutineXDebuggerManagerListener.kt
+++ idea/jvm-debugger/jvm-debugger-core/src/org/jetbrains/kotlin/idea/debugger/coroutines/CoroutineXDebuggerManagerListener.kt
@@ -51,7 +51,7 @@ class CoroutineXDebuggerManagerListener(val project: Project) : XDebuggerManager
      * Adds panel to XDebugSessionTab
      */
     private fun registerCoroutinesPanel(ui: RunnerLayoutUi, session: DebuggerSession) {
-        val panel = CoroutinesPanel(session.project, session.contextManager)
+        val panel = CoroutinesPanel(project, session.contextManager)
         val content = ui.createContent(
             "CoroutinesContent", panel, "Coroutines", // TODO(design)
             AllIcons.Debugger.ThreadGroup, null
